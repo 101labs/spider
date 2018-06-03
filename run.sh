35d6224b0c19ae30be3a898d7e6a4bac3518b821
杨一博 <yangyibo@yidian-inc.com>
2018-06-04 00:26:07 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 #mvn compile
-#mvn exec:java -Dexec.mainClass="org.decaywood.test.Stock_yyb"
-java -cp target/XueQiuSpider-1.0-SNAPSHOT.jar org.decaywood.test.Stock_yyb
+mvn exec:java -Dexec.mainClass="org.decaywood.test.Stock_yyb"
+#mvn clean package -DskipTests=true
+#java -cp target/XueQiuSpider-1.0-SNAPSHOT.jar org.decaywood.test.Stock_yyb
